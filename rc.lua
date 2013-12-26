b892e225096caa8e9e3b2503e97dd04cc03b0d78
Quan Guo <guotsuan@gmail.com>
2013-12-26 13:13:08 +0100

    clean the old codes

--- rc.lua
+++ rc.lua
@@ -26,10 +26,6 @@ local drop = require("scratchdrop")
 --require("aweror")
 --require("shifty")
 
-g_old = 0
-d_old = 0
-m_old = 0
-a_old = 0
 
 ilog = lognotify{
     logs = { pacman = { file = "/var/log/pacman.log"},
@@ -1284,37 +1280,7 @@ function usefuleval(s)--{{{
 end--}}}
 
 
----- seed and "pop a few"
---
---math.randomseed( os.time())
-
---for i=1,1000 do tmp=math.random(0,1000) end
-
---x = 0
-
----- setup the timer
---mytimer = timer { timeout = x }
---mytimer:connect_signal("timeout", function()
-
-  ---- tell awsetbg to randomly choose a wallpaper from your wallpaper directory
-  os.execute("feh --bg-fill -z -r ~/.config/wallpapers &")
-
-  ---- stop the timer (we don't need multiple instances running at the same time)
-  --mytimer:stop()
-
-  ---- define the interval in which the next wallpaper change should occur in seconds
-  ---- (in this case anytime between 10 and 20 minutes)
-  --x = math.random(600, 1200)
-
-  ----restart the timer
-  --mytimer.timeout = x
-  --mytimer:start()
-    --end)
-
----- initial start when rc.lua is first run
---mytimer:start()
--- }}}
-
+os.execute("feh --bg-fill -z -r ~/.config/wallpapers &")
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
